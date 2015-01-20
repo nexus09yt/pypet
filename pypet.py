239a1bfd97c7085148e2d4301402fb1963dc1531
Tatiana Tylosky <tatianatylosky@gmail.com>
2015-01-19 23:11:12 -0500

    Update
    Add new code based on previous changes to guide

--- pypet.py
+++ pypet.py
@@ -1,32 +1,38 @@
-print 'Welome to Pypet!'
+print 'Welcome to Pypet!'
 
 cat = {
-  'name': 'Mr Fluffy',
+  'name': 'Fluffy',
   'hungry': True,
   'weight': 9.5,
-  'number_of_toys': 5,
-  'photo': '(=^o.o^=)__',
+  'age': 5,
+  'photo': '(=^o.o^=)_',
 }
 
 mouse = {
-  'name': 'Mouse',
-  'hungry': False,
-  'weight': 1.5,
-  'number_of_toys': 6,
-  'photo': '<:3 )~~~~',
+    'name': 'Mouse',
+    'age': 6,
+    'weight': 1.5,
+    'hungry': False,
+    'photo': '<:3 )~~~~',
 }
 
 pets = [cat, mouse]
 
-def feed(cat):
-	if cat['hungry']:
-		cat['hungry'] = False
-		cat['weight']+=1
-	else:
-		print 'The Pypet is not hungry!'
+
+def feed(pet):
+  if pet['hungry'] == True:
+    pet['hungry'] = False
+    pet['weight'] = pet['weight'] + 1
+    print 'omnomom!!'
+  else:
+    print 'The Pypet is not hungry!'
 
 for pet in pets:
-  feed(pet)
-  print pet
-
+    print '------------------------------'
+    print 'Hello ' + pet['name'] + '!'
+    print pet['photo']
+    print 'Weight: ' + str(pet['weight'])
+    feed(pet)
+    print 'Weight: ' + str(pet['weight'])
+    print '------------------------------'
 
